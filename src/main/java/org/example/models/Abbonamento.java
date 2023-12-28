9057af840d41ac89f79ead5838417b12ae282b9d
Enrico <enricobergamini@outlook.it>
2023-12-28 12:29:41 +0100

    Formatted text

--- src/main/java/org/example/models/Abbonamento.java
+++ src/main/java/org/example/models/Abbonamento.java
@@ -1,15 +1,18 @@
 package org.example.models;
+
 import java.time.LocalDate;
+
 public class Abbonamento {
     private int id_abbonamento, id_rivista, id_utente;
     LocalDate data_inizio, data_fine;
-    public Abbonamento(int abbonamento, int rivista, int utente, LocalDate inizio, LocalDate fine){
 
-        id_abbonamento=abbonamento;
-        id_rivista=rivista;
-        id_utente=utente;
-        data_inizio=inizio;
-        data_fine=fine;
+    public Abbonamento(int abbonamento, int rivista, int utente, LocalDate inizio, LocalDate fine) {
+
+        id_abbonamento = abbonamento;
+        id_rivista = rivista;
+        id_utente = utente;
+        data_inizio = inizio;
+        data_fine = fine;
 
 
     }
@@ -29,27 +32,23 @@ public class Abbonamento {
         return id_abbonamento;
     }
 
-    public LocalDate getData_inizio() { return data_inizio;}
-
-    public LocalDate getData_fine() { return data_fine;}
-
-    public void setId_abbonamento(int id_abbonamento) {
-        this.id_abbonamento = id_abbonamento;
+    public LocalDate getData_inizio() {
+        return data_inizio;
     }
+
+    public LocalDate getData_fine() {
+        return data_fine;
+    }
+
 
     public int getId_rivista() {
         return id_rivista;
     }
 
-    public void setId_rivista(int id_rivista) {
-        this.id_rivista = id_rivista;
-    }
 
     public int getId_utente() {
         return id_utente;
     }
 
-    public void setId_utente(int id_utente) {
-        this.id_utente = id_utente;
-    }
+
 }
--- src/main/java/org/example/models/Utente.java
+++ src/main/java/org/example/models/Utente.java
@@ -1,18 +1,20 @@
 package org.example.models;
+
 import java.time.LocalDate;
+
 public class Utente {
 
     private int ID, DocID;
     private String Nome, Cognome, Indirizzo;
     private LocalDate Nascita;
 
-    public Utente(int id_utente, String nome_utente, String Cognome_utente, LocalDate nascita_utente, String indirizzo_utente, int docID_utente){
-        ID=id_utente;
-        DocID=docID_utente;
-        Nome=nome_utente;
-        Cognome=Cognome_utente;
-        Indirizzo=indirizzo_utente;
-        Nascita=nascita_utente;
+    public Utente(int id_utente, String nome_utente, String Cognome_utente, LocalDate nascita_utente, String indirizzo_utente, int docID_utente) {
+        ID = id_utente;
+        DocID = docID_utente;
+        Nome = nome_utente;
+        Cognome = Cognome_utente;
+        Indirizzo = indirizzo_utente;
+        Nascita = nascita_utente;
     }
 
 
@@ -32,47 +34,30 @@ public class Utente {
         return ID;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
-    }
 
     public int getDocID() {
         return DocID;
     }
 
-    public void setDocID(int docID) {
-        DocID = docID;
-    }
 
     public String getNome() {
         return Nome;
     }
 
-    public void setNome(String nome) {
-        Nome = nome;
-    }
 
     public String getCognome() {
         return Cognome;
     }
 
-    public void setCognome(String cognome) {
-        Cognome = cognome;
-    }
 
     public String getIndirizzo() {
         return Indirizzo;
     }
 
-    public void setIndirizzo(String indirizzo) {
-        Indirizzo = indirizzo;
-    }
 
     public LocalDate getNascita() {
         return Nascita;
     }
 
-    public void setNascita(LocalDate nascita) {
-        Nascita = nascita;
-    }
+
 }
